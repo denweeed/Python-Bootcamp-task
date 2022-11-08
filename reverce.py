06536293ac844d784b252093eba34ba6262247f1
denweeed <112696724+denweeed@users.noreply.github.com>
2022-11-08 13:06:06 +0200

    Update reverce.py

--- reverce.py
+++ reverce.py
@@ -1,10 +1,4 @@
-def reverse(any_string):
-    if any_string == "":
-        return any_string
-    else:
-        return reverse(any_string[1:]) + any_string[:1]
-
-
 if __name__ == "__main__":
-    s = "string"
-    print(reverse(s))
+    s = "String"
+    rev_s = s[::-1]
+    print(rev_s)
